372b05630b6a98d1195d838fec873f5ed53730e8
Arenozy <arenozymandias@gmail.com>
2020-09-26 11:21:21 -0400

    Finished tile game, will need to come back later and hard code a start point and a reset to start button

--- Assets/Scripts/LightsOut.cs
+++ Assets/Scripts/LightsOut.cs
@@ -2,16 +2,21 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class LightsOut : MonoBehaviour
 {
     // Start is called before the first frame update
 
-    public List<List<bool>> Grid = new List<List<bool>>();
+    public List<List<GameObject>> Grid = new List<List<GameObject>>();
     public int GridSize;
     public GameObject GridRowPrefab;
     public GameObject Tile;
     public GameObject content;
+
+    [SerializeField] Color colorLit;
+    [SerializeField] Color colorDark;
+
 
     void Start()
     {
@@ -36,14 +41,81 @@ public class LightsOut : MonoBehaviour
             GameObject newRow = Instantiate(GridRowPrefab, content.transform);
             Transform newRowContent = newRow.transform.GetChild(0).GetChild(0);
             Debug.Log(newRowContent.name);
-            List<bool> gridRow = new List<bool>();
+            List<GameObject> gridRow = new List<GameObject>();
             for (int j = 0; j < GridSize; j++)
             {
-                bool test = true;
-                gridRow.Add(test);
                 GameObject tile = Instantiate(Tile, newRowContent);
+                tile.GetComponent<LOtile>().SetPos(i, j);
+                gridRow.Add(tile);
             }
             Grid.Add(gridRow);
+        }
+    }
+
+    public void FlipLights(int colPos, int rowPos)
+    {
+        List<GameObject> rowList = Grid[colPos];
+        GameObject selectedTile = rowList[rowPos];
+        Flip(selectedTile);
+
+        // Get E tile
+        if(rowPos < rowList.Count - 1)
+        {
+            selectedTile = rowList[rowPos+1];
+            Flip(selectedTile);
+        }
+
+        // Get W tile
+        if (rowPos > 0)
+        {
+            selectedTile = rowList[rowPos - 1];
+            Flip(selectedTile);
+        }
+
+        // Get N tile
+        if (colPos > 0)
+        {
+            rowList = Grid[colPos-1];
+            selectedTile = rowList[rowPos];
+            Flip(selectedTile);
+        }
+
+        // Get S tile
+        if (colPos < Grid.Count-1)
+        {
+            rowList = Grid[colPos + 1];
+            selectedTile = rowList[rowPos];
+            Flip(selectedTile);
+        }
+
+        bool win = true;
+        foreach(List<GameObject> row in Grid)
+        {
+            foreach(GameObject tile in row)
+            {
+                if (tile.GetComponent<Image>().color == colorDark)
+                {
+                    win = false;
+                }
+            }
+        }
+        if(win)
+        {
+            Debug.Log("Yay");
+        }
+    }
+
+    public void Flip(GameObject tile)
+    {
+        Image tileImage = tile.GetComponent<Image>();
+
+        if(tileImage.color == colorLit)
+        {
+            tileImage.color = colorDark;
+        }
+        else
+        {
+            tileImage.color = colorLit;
         }
     }
 }
